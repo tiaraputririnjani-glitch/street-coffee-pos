ad8001c421e9a6946bfd2e1e74435e4b29cbfac4
Tiara Putri Rinjani <tiaraputririnjani@gmail.com>
2026-02-18 15:57:26 +0700

    daftarkan route logout dan auth

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,16 @@
 <?php
-use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TransaksiController;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/', [TransaksiController::class, 'index'])->name('dashboard');
+// Halaman Login
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout'); // INI YANG BIKIN EROR TADI
 
-
-Route::post('/checkout', [TransaksiController::class, 'store'])->name('checkout');
+// Dashboard (Hanya bisa dibuka kalau sudah LOGIN)
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', [TransaksiController::class, 'index'])->name('dashboard');
+    Route::post('/checkout', [TransaksiController::class, 'checkout'])->name('checkout');
+});
